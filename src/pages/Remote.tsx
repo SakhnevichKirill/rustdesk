a773dfc86ec89566576dcb226f0c10907abf55f7
artemkons <konstantinov050701@gmail.com>
2022-12-04 11:08:19 +0300

    Add Remote page

--- src/pages/Remote.tsx
+++ src/pages/Remote.tsx
@@ -1,28 +1,18 @@
 import React from 'react'
 import { invoke } from '@tauri-apps/api'
-import { emit, listen } from '@tauri-apps/api/event'
+import { listen } from '@tauri-apps/api/event'
 
 const Remote = () => {
     invoke('reconnect')
         .then(() => {
-            listen('native-remote', (e) => {
-                console.log(e)
+            listen('native-remote', (e:any) => {
+                console.log(e.payload)
             })
         })
 
-    // TEST event
-    listen('click', (event) => {
-        // console.log(event)
-    })
-
-    // emits the `click` event with the object payload
-    emit('click', {
-        theMessage: 'Tauri is awesome!',
-    })
-    
     return (
         <div>
-            Hi
+            Remote
         </div>
     )
 }
