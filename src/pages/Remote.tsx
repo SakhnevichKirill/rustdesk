88329570cb60cdad6b0a2eb6148216599c938b79
artemkons <konstantinov050701@gmail.com>
2022-12-08 11:41:53 +0300

    Add remote screen

--- src/pages/Remote.tsx
+++ src/pages/Remote.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { invoke } from '@tauri-apps/api'
 import { listen } from '@tauri-apps/api/event'
 
 const Remote = () => {
-    const [width, setWidth] = useState(0)
+    const [remoteDim, setRemoteDim] = useState({ width: 0, height: 0 })
+    const [pixels, setPixels] = useState<Uint8ClampedArray>(new Uint8ClampedArray([0]))
 
     useEffect(() => {
         const listenEvents = async () => {
             await invoke('reconnect')
 
             const unlistenSetDisplay = await listen('setDisplay', (e: { payload: [x: number, y: number, w: number, h: number] }) => {
-                    setWidth(e.payload[2])
+                    setRemoteDim({ width: e.payload[2], height: e.payload[3] })
                 })
-            const unlistenNativeRemote = await listen('native-remote', (e:any) => {
-                    console.log(e)
+            const unlistenNativeRemote = await listen('native-remote', (e: { payload: Uint8ClampedArray }) => {
+                    setPixels(new Uint8ClampedArray(e.payload))
                 })
 
             return {unlistenSetDisplay, unlistenNativeRemote}
@@ -30,10 +31,22 @@ const Remote = () => {
            }) 
         }
     }, [])
+
+    useEffect(() => {
+        const { width, height} = remoteDim
+        if (width && height && pixels.length > 1) {
+            const imageData = new ImageData(pixels, width)
+            const canvas = document.getElementById('canvas') as HTMLCanvasElement
+            const ctx = canvas?.getContext('2d');
+            if (ctx) {
+                ctx.putImageData(imageData, 0, 0)
+            }
+        }
+    }, [remoteDim, pixels])
     
     return (
         <div>
-            Remote {width}
+            <canvas id="canvas" {...remoteDim}></canvas>
         </div>
     )
 }
