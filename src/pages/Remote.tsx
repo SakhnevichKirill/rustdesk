77d6ff8ca3a6efb7300716cce65cc0373a5b6695
artemkons <konstantinov050701@gmail.com>
2022-12-26 12:38:13 +0300

    Just test vp9

--- src/pages/Remote.tsx
+++ src/pages/Remote.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useMemo } from 'react'
 
 import { Box, Center, CircularProgress, Text } from '@chakra-ui/react'
 
@@ -10,59 +10,92 @@ const Remote = () => {
     const [msg, setMsg] = useState("")
     const [connectionSpeed, setConnectionSpeed] = useState("")
 
+    const videoRef = useRef<HTMLVideoElement | null>(null)
+    const mediaSource = useMemo(() => new MediaSource(), [])
+    let mediaSrcBuffer: SourceBuffer | null = null
+
     const [remoteDim, setRemoteDim] = useState({ width: 0, height: 0 })
     const [pixels, setPixels] = useState<Uint8ClampedArray>(new Uint8ClampedArray([0]))
+
+    useEffect(() => {
+        const video = videoRef.current
+        if ('MediaSource' in window && video) {
+            video.src = URL.createObjectURL(mediaSource)
+            mediaSource.addEventListener('sourceopen', handleSourceOpen)
+            return () => {
+                mediaSource.removeEventListener('sourceopen', handleSourceOpen)
+            };
+        } else
+            console.error('Doesnt support MediaSource')
+    }, [mediaSource])
+
+    const handleSourceOpen = () => {
+       mediaSrcBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp9"')
+    }
 
     useEffect(() => {
         const listenEvents = async () => {
             setConnecitonLoading(true)
             await invoke('reconnect')
+            await invoke('record_screen')
+
+            // FIXME for every tauri e type I need to e: {payload: {...}}
+            const unlistenEncodedFrames = await listen('encoded_frames', (e: { payload: { data: number[] }[] }) => {
+                console.log(e)
+                // TODO могут данные прийти, а буфера нет открылось?
+                if (mediaSrcBuffer) {
+                    const binArr = new Uint8Array(e.payload[0].data)
+                    mediaSrcBuffer.appendBuffer(binArr.buffer)
+                }
+            })
 
             const unlistenMsgboxRetry = await listen('msgbox_retry', (e: {
                 payload: [
                     // FIXME Im not sure about this fields names
                     status: string, statusMsg: string, connectionMsg: string, idkMsg: string, idkBool: boolean
                 ]
-                }) => {
-                    const status = e.payload[0]
-                    if (status === 'success') {
-                        setConnecitonLoading(false)
-                        setMsg("")
-                    }
-                    if (status === 'input-password') setMsg('Подтвердите подключение')
-                })
+            }) => {
+                const status = e.payload[0]
+                if (status === 'success') {
+                    setConnecitonLoading(false)
+                    setMsg("")
+                }
+                if (status === 'input-password') setMsg('Подтвердите подключение')
+            })
             const unlistenSetDisplay = await listen('setDisplay', (e: { payload: [x: number, y: number, w: number, h: number] }) => {
-                    setRemoteDim({ width: e.payload[2], height: e.payload[3] })
-                })
+                setRemoteDim({ width: e.payload[2], height: e.payload[3] })
+            })
             const unlistenNativeRemote = await listen('native-remote', (e: { payload: Uint8ClampedArray }) => {
-                    setPixels(new Uint8ClampedArray(e.payload))
-                })
-            const unlistenUpdateQualityStatus = await listen('updateQualityStatus', (e: {payload: string[]}) => {
-                    setConnectionSpeed(e.payload[0])
-                })
+                setPixels(new Uint8ClampedArray(e.payload))
+            })
+            const unlistenUpdateQualityStatus = await listen('updateQualityStatus', (e: { payload: string[] }) => {
+                setConnectionSpeed(e.payload[0])
+            })
 
-            return {unlistenSetDisplay, unlistenNativeRemote, unlistenMsgboxRetry, unlistenUpdateQualityStatus}
+            return { unlistenSetDisplay, unlistenNativeRemote, unlistenMsgboxRetry, unlistenUpdateQualityStatus, unlistenEncodedFrames }
         }
 
         const unlisten = listenEvents().catch(() => null)
 
         return () => {
-           unlisten.then(unl => {
-              if (unl) {
-                  unl.unlistenNativeRemote()
-                  unl.unlistenSetDisplay()
-                  unl.unlistenMsgboxRetry()
-                  unl.unlistenUpdateQualityStatus()
-              } 
-           }) 
+            // FIXME It's awkward write every func, better to use array
+            unlisten.then(unl => {
+                if (unl) {
+                    unl.unlistenNativeRemote()
+                    unl.unlistenSetDisplay()
+                    unl.unlistenMsgboxRetry()
+                    unl.unlistenUpdateQualityStatus()
+                    unl.unlistenEncodedFrames()
+                }
+            })
         }
     }, [])
 
     useEffect(() => {
-        const { width, height} = remoteDim
+        const { width, height } = remoteDim
         if (width && height && pixels.length > 1) {
             const imageData = new ImageData(pixels, width, height, {
-                colorSpace: "srgb" 
+                colorSpace: "srgb"
             })
             const canvas = document.getElementById('canvas') as HTMLCanvasElement
             const ctx = canvas?.getContext('2d');
@@ -71,17 +104,18 @@ const Remote = () => {
             }
         }
     }, [remoteDim, pixels])
-    
+
     return (
         <Center h="100vh">
             <Text pos='absolute' left={0} top={0} color='red'>{connectionSpeed}</Text>
             <Box height='100%'>
                 {connectionLoading ?
-                    <>"Подключаемся... "<CircularProgress isIndeterminate/></> :
+                    <>"Подключаемся... "<CircularProgress isIndeterminate /></> :
                     <canvas id="canvas" width={1690} height={1122}></canvas>
                 }
                 <Text>{msg}</Text>
             </Box>
+            <video ref={videoRef}/>
         </Center>
     )
 }
