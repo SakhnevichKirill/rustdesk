328cad7166df17cca290b506d6f4a28e63c030cb
KirSR <kirsr.life@gmail.com>
2023-02-22 17:14:20 +0300

    todo: getConnectStatusStr, contextMenu, msgbox popup

--- src/pages/Remotes.tsx
+++ src/pages/Remotes.tsx
@@ -11,6 +11,94 @@ const isRendezvousServiceStopped = async () => {
     return await invoke<string>("get_option", {key: "stop-rendezvous-service"}) === "Y"
 }
 
+const handler_msgbox = (type: string, title: string, text: string, link: string = "", hasRetry=false) => {
+    // crash somehow (when input wrong password), even with small time, for example, 1ms
+    setTimeout(() => {msgbox(type, title, text, link, null, 180, 500, hasRetry);}, 60);
+}
+
+const msgbox = async (type: string, title: string, content: string, link: string="", callback: any=null, height=180, width=500, hasRetry=false, contentStyle="") => {
+    // $(body).scrollTo(0, 0);
+    if (!type) {
+        // closeMsgbox();
+        return;
+    }
+    let remember = false;
+    try { remember = await invoke("get_remember"); } catch(e) {}
+    let auto_login = false;
+    try { auto_login = await invoke("get_option", {key: "auto-login"}) != ''; } catch(e) {}
+    // width += is_xfce ? 50 : 0;
+    // height += is_xfce ? 50 : 0;
+
+    if (type.indexOf("input-password") >= 0) {
+        callback = function (res: { password: any; remember: any }) {
+            if (!res) {
+                // view.close();
+                return;
+            }
+            invoke("login", {password: res.password, remember: res.remember}).then(); 
+            // if (!is_port_forward) {
+            //   // Specially handling file transfer for no permission hanging issue (including 60ms
+            //   // timer in setPermission.
+            //   // For wrong password input hanging issue, we can not use handler.msgbox.
+            //   // But how about wrong password for file transfer?
+            //   if (is_file_transfer) handler_msgbox("connecting", "Connecting...", "Logging in...");
+            //   else msgbox("connecting", "Connecting...", "Logging in...");
+            // }
+        };
+    // } else if (type.indexOf("custom") < 0 && !is_port_forward && !callback) {
+        // callback = function() { view.close(); }
+    } else if (type == 'wait-remote-accept-nook') {
+        callback = function (res: any) {
+            if (!res) {
+                // view.close();
+                return;
+            }
+        };
+    }
+    let last_msgbox_tag = type + "-" + title + "-" + content + "-" + link;
+    // $(#msgbox).content(<MsgboxComponent width={width} height={height} auto_login={auto_login} type={type} title={title} content={content} link={link} remember={remember} callback={callback} contentStyle={contentStyle} hasRetry={hasRetry} />);
+}
+
+const contextMenu = async () => {
+    let configOptions: any = await invoke("get_options");
+    let old_relay = configOptions["relay-server"] || "";
+    let old_api = configOptions["api-server"] || "";
+    let old_id = configOptions["custom-rendezvous-server"] || "";
+    let old_key = configOptions["key"] || "";
+    msgbox("custom-server", "ID/Relay Server", "<div .form .set-password> \
+    <div><span>" + "ID Server" + ": </span><input|text .outline-focus name='id' value='" + old_id + "' /></div> \
+    <div><span>" + "Relay Server" + ": </span><input|text name='relay' value='" + old_relay + "' /></div> \
+    <div><span>" + "API Server" + ": </span><input|text name='api' value='" + old_api + "' /></div> \
+    <div><span>" + "Key" + ": </span><input|text name='key' value='" + old_key + "' /></div> \
+    </div> \
+    ", "", async (res: any=null) => {
+        if (!res) return;
+        let id = (res.id || "").trim();
+        let relay = (res.relay || "").trim();
+        let api = (res.api || "").trim().toLowerCase();
+        let key = (res.key || "").trim();
+        if (id == old_id && relay == old_relay && key == old_key && api == old_api) return;
+        if (id) {
+            let err = await invoke("test_if_valid_server", {host: id});
+            if (err) return "ID Server" + ": " + err;
+        }
+        if (relay) {
+            let err = await invoke("test_if_valid_server", {host: relay});
+            if (err) return "Relay Server" + ": " + err;
+        }
+        if (api) {
+            if (0 != api.indexOf("https://") && 0 != api.indexOf("http://")) {
+                return  "API Server" + ": " + "invalid_http";
+            }
+        }
+        configOptions["custom-rendezvous-server"] = id;
+        configOptions["relay-server"] = relay;
+        configOptions["api-server"] = api;
+        configOptions["key"] = key;
+        await invoke("set_options", {m: configOptions});
+    }, 260);
+}
+
 const Remotes = () => {    
     const [remoteId, setRemoteId] = useState<string>('')
     const [serviceStopped, setServiceStopped] = useState<boolean>()
@@ -129,8 +217,11 @@ const Remotes = () => {
 
         const listenEvents = async () => {
             console.log("keyConfirmed: ", keyConfirmed)
+
             if (keyConfirmed) {
                 updateId()
+            } else {
+                console.log("Generating ...")
             }
             let is_can_screen_recording = await invoke<boolean>("is_can_screen_recording", { prompt: false })
             console.log("is_can_screen_recording: ", is_can_screen_recording)
@@ -152,6 +243,10 @@ const Remotes = () => {
     // Inits the app
     useEffect(() => {
         const listenEvents = async () => {
+            // TODO: implement a menu to configure the server
+            // TODO: implement msgbox popup
+            // contextMenu().then()
+
             setServiceStopped(await isServiceStopped())
             setRendezvousServiceStopped(await isRendezvousServiceStopped())
             setUsingPublicServer(await invoke<boolean>("using_public_server"))
@@ -168,7 +263,7 @@ const Remotes = () => {
             unlisten.then((unl) => {    
                 console.log(unl)  
             }) 
-         }
+        }
     }, [])
 
     // This function calculate X and Y
@@ -195,6 +290,19 @@ const Remotes = () => {
         await invoke("closing", {x: 0, y: 0, w: dimensions.width, h: dimensions.height})
         console.log(dimensions.width, dimensions.height)
     })
+
+    // TODO: show connection status
+    const getConnectStatusStr = async () => {
+        if (serviceStopped) {
+            return "Service is not running";
+        } else if (connectStatus === -1) {
+            return 'not_ready_status';
+        } else if (connectStatus === 0) {
+            return 'connecting_status';
+        }
+        if (!await invoke("using_public_server")) return 'Ready';
+        return <span>{"Ready"}, <span>{"setup_server_tip"}</span></span>;
+    }
 
     const createNewConnect = (id: string, type: string) => {
         const _id = id.replace(/\s/g, "")
